4bd714d1a180a70636b16e46b4b01c8da528d04c
Jeykobz <jakobstock@web.de>
2021-04-15 17:55:14 +0200

    new writer class represents object resposible for structured output

---
+++ writer.py
@@ -0,0 +1,20 @@
+class Writer():
+    """
+    Represents objects responsible for a structured and clear output
+
+    Attributes:
+        _outputs: list of lists containing all features for each layer
+        _total: list of all features summarized
+        _durations: array of duration of each layer
+    """
+
+    def __init__(self):
+        """Inits Writer with empty lists which will be filled by analysing methods"""
+        self._outputs = []
+        self._total = []
+        self._durations = []
+
+    def printout(self):
+        """prints collected features"""
+        for idx in range(0, len(self._outputs)):
+            print(self._outputs[idx])
